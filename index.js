67139ea089dc7b184c5783d24665bce94cecced3
Amanze Bruno <heroicemblem69@gmail.com>
2024-10-06 14:59:46 +0100

    Done wth the digital clock

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateClock(){
+    const now = new Date()
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const meridem = hours >= 12 ? "PM" : "AM"
+    
+
+    const joinString = `${hours}:${minutes}:${seconds} ${meridem}`
+
+    document.getElementById("clock").textContent = `${joinString}`
+}
+
+updateClock()
+setInterval(updateClock, 1000);
